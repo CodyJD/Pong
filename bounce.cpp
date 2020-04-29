91e226f60bcdaf21b27f1f31a747e0acfc676a34
itsangelatante <theangelat@gmail.com>
2020-04-29 09:56:18 -0700

    game board + net added and broken score board

--- bounce.cpp
+++ bounce.cpp
@@ -10,77 +10,108 @@ const int W = 80;
 const int H = 20;
 
 class Ball {
-    public:
-        double pos[2];
-        double vel[2];
+public:
+  double pos[2];
+  double vel[2];
 
-        Ball () {
-          pos[0]=W/2;
-          pos[1]= H/2;
-          vel[0]= vel[1]=0;
-        }
+  Ball () {
+    pos[0]=W/2;
+    pos[1]= H/2;
+    vel[0]= vel[1]=0;
+  }
 };
 
 class Paddle {
-    public:
-      double pos[2];
+public:
+  double pos[2];
 
-    Paddle () {
-      pos[0]= W;
-      pos[1]= H/2;
-    }
+  Paddle () {
+    pos[0]= W;
+    pos[1]= H/2;
+  }
 };
 
 void calculateDir(int W, Ball &b) {
-    b.pos[0] += b.vel[0];
-    b.pos[1] += b.vel[1];
+  b.pos[0] += b.vel[0];
+  b.pos[1] += b.vel[1];
 
-    if (b.pos[0] >= W - 2) {
-        b.vel[0] = -b.vel[0];
-        b.pos[0] = W-2;
-    }
-    if (b.pos[0] < 0) {
-        b.pos[0] = 1;
-        b.vel[0] = -b.vel[0];
-    }
+  // b.pos[0] will eventually be changed to add to score rather than
+  // just bouncing
+  if (b.pos[0] >= W - 2) {
+    b.vel[0] = -b.vel[0];
+    b.pos[0] = (W-2); //makes the ball bounce away from the right wall
+    b.pos[1] += 3;
+  }
+  if (b.pos[0] < 1) {
+    b.pos[0] = 1;
+    b.vel[0] = -b.vel[0];
+    b.pos[1] -= 8;
+  }
+  if (b.pos[1] >= (H-1)) {
+    b.vel[1] = -b.vel[1];
+    b.pos[1] = H - 2;
+  }
+  if (b.pos[1] < 2) {
+    b.vel[1] = -b.vel[1];
+    b.pos[1] = -b.pos[1];
+  }
 }
 
 void show_arr(char arr[][W], int H, int W) {
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            cout << arr[i][j];
-        }
-        cout << endl;
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
+      cout << arr[i][j];
     }
     cout << endl;
+  }
+  cout << endl;
 }
 
 void make_gameBoard(char arr[][W]) {
   for (int i = 0; i < H; i++) {
-      for (int j = 0; j < W; j++) {
-          arr[i][j] = '.';
+    for (int j = 0; j < W; j++) {
+      arr[i][j] = '.';
+      if (i == 0 || i == (H-1)) {
+        arr[i][j] = '=';
       }
+      if (j == 0 || j == (W-1)) {
+        arr[i][j] = '|';
+      }
+      if (j == (W/2)-1 || j == (W/2)) {
+        arr[i][j] = '|';
+      }
+      if ((j == (W/2)-1 || j == (W/2)) && i == 0) {
+        arr[i][j-2] = '[';
+        arr[i][j-1] = ']';
+
+        arr[i][j-5] = '[';
+        arr[i][j-4] = ']';
+
+        arr[i][j-7] = '[';
+        arr[i][j-6] = ']';
+      }
+    }
   }
 }
 
 int main(void) {
-    char gameBoard[H][W];
-    Ball b;
-    bool quit = false;
+  char gameBoard[H][W];
+  Ball b;
+  bool quit = false;
 
 
-    b.vel[0]=2.0;
-    do {
-        make_gameBoard(gameBoard);
+  b.vel[0] = 3.0;
+  do {
+    make_gameBoard(gameBoard);
 
-        gameBoard[(int)b.pos[1]][(int)b.pos[0]] = '[';
-        gameBoard[(int)b.pos[1]][(int)b.pos[0]+1] = ']';
+    gameBoard[(int)b.pos[1]][(int)b.pos[0]] = '[';
+    gameBoard[(int)b.pos[1]][(int)b.pos[0]+1] = ']';
 
-        show_arr(gameBoard,H,W);
-        usleep(100000);
-        calculateDir(W, b);
+    show_arr(gameBoard,H,W);
+    usleep(100000);
+    calculateDir(W, b);
 
-    } while (!quit);
+  } while (!quit);
 
-    return 0;
+  return 0;
 }
